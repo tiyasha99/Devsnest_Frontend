ccf571959e7f387e2d60ca59c2a44f470cb17189
tiyasha99 <69319639+tiyasha99@users.noreply.github.com>
2021-07-01 16:23:21 +0530

    Analog clock

---
+++ DAY4/script.js
@@ -0,0 +1,15 @@
+const s = document.getElementById('s');
+const h = document.getElementById('hr');
+const m = document.getElementById('mn');
+
+setInterval(updateClock,1000);
+function updateClock(){
+  let d= new Date();
+  let sec = d.getSeconds() * 6;
+  let min = d.getMinutes() * 6;
+  let hour = d.getHours() * 30 + min/12;
+
+  s.style.transform = `rotateZ(${sec}deg)`;
+  m.style.transform = `rotateZ(${min}deg)`;
+  h.style.transform = `rotateZ(${hour}deg)`
+}
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ DAY-01: Cover Letter in simple html.
 
 DAY-02: Using css to style DAY-01 cover letter
 
-Day-03:
+Day-03: Portfolio website using HTML,CSS and JavaScript
